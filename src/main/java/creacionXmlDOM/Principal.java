605ef1c889c4cec9121cee25dd3ebffd3c5b8dc0
Morad <Morad-96@hotmail.com>
2022-11-08 13:53:45 +0100

    second commit

--- src/main/java/creacionXmlDOM/Principal.java
+++ src/main/java/creacionXmlDOM/Principal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package creacionXml;
+package creacionXmlDOM;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -36,67 +36,65 @@ public class Principal {
         Alumno alumno1 = new Alumno(1, "Morad", 26);
         Alumno alumno2 = new Alumno(2, "Paul", 25);
         Alumno alumno3 = new Alumno(3, "Juanma", 20);
-
+        Alumno alumno4 = new Alumno(4, "Andrew", 31);
         listaAlumnos.add(alumno1);
         listaAlumnos.add(alumno2);
         listaAlumnos.add(alumno3);
+        listaAlumnos.add(alumno4);
 
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             DOMImplementation dom = db.getDOMImplementation();
             //duda si importar con org o javax
-            Document document = dom.createDocument(null,"xml",null);
+            Document document = dom.createDocument(null, "xml", null);
             //se crea el primer nodo
             Element raiz = document.createElement("alumnos");
             document.getDocumentElement().appendChild(raiz);
-            
+
             //creamos el arbol
-            Element nodoAlumno,nodoDatos; 
+            Element nodoAlumno, nodoDatos;
             Text texto;
-            
+
             for (Alumno listaAlumno : listaAlumnos) {
-                
+
                 nodoAlumno = document.createElement("alumno");
                 raiz.appendChild(nodoAlumno);
-                
-                nodoDatos= document.createElement("Id");
+
+                nodoDatos = document.createElement("Id");
                 nodoAlumno.appendChild(nodoDatos);
                 texto = document.createTextNode(String.valueOf(listaAlumno.getId()));
                 nodoDatos.appendChild(texto);
-                
-                nodoDatos= document.createElement("Nombre");
+
+                nodoDatos = document.createElement("Nombre");
                 nodoAlumno.appendChild(nodoDatos);
                 texto = document.createTextNode(listaAlumno.getNombre());
                 nodoDatos.appendChild(texto);
-                
-                
+
                 nodoDatos = document.createElement("Edad");
                 nodoAlumno.appendChild(nodoDatos);
                 texto = document.createTextNode(String.valueOf(listaAlumno.getEdad()));
                 nodoDatos.appendChild(texto);
-                
+
                 //objetos para escribir en el fichero
-                
                 Source source = new DOMSource(document);
                 Result result = new StreamResult(f);
-                
-                try{ 
-                Transformer transformer = TransformerFactory.newInstance().newTransformer();
-                transformer.setOutputProperty("indent","yes");
-                //ver bien la linea de arriba
-                //escribimos en el fichero
-                transformer.transform(source, result);
-                        
-                }catch(TransformerException ex){System.out.println(ex.getMessage());}
+
+                try {
+                    Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                    transformer.setOutputProperty("indent", "yes");
+                    //ver bien la linea de arriba
+                    //escribimos en el fichero
+                    transformer.transform(source, result);
+
+                } catch (TransformerException ex) {
+                    System.out.println(ex.getMessage());
+                }
             }
-            
-            
-            
-        
+
         } catch (ParserConfigurationException e) {
             System.out.println(e.getMessage());
-        } 
+        }
 
     }
 
---
+++ src/main/java/lecturaXmlSAX/Principal.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lecturaXmlSAX;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+/**
+ *
+ * @author alumnotd
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+
+        File f = new File("alumnos.xml");
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(f);
+//importar con org
+            NodeList alumnos = document.getElementsByTagName("alumno");
+            Node nodoAlumno;
+            Element nodoDatos;
+            
+            for (int i = 0; i < alumnos.getLength(); i++) {
+                nodoAlumno = alumnos.item(i);
+                nodoDatos =(Element) nodoAlumno;
+                //imprimir los valores
+                System.out.println("Id: "+ nodoDatos.getElementsByTagName("Id").item(0).getTextContent());
+                System.out.println("Nombre: "+ nodoDatos.getElementsByTagName("Nombre").item(0).getTextContent());
+                System.out.println("Edad: "+ nodoDatos.getElementsByTagName("Edad").item(0).getTextContent());
+            }
+            
+        } catch (ParserConfigurationException e) {
+            System.out.println(e.getMessage());
+        } catch (SAXException s) {
+            System.out.println(s.getMessage());
+        }catch (IOException io) {
+            System.out.println(io.getMessage());
+        } 
+    }
+}
